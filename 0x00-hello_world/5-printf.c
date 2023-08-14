322bd3d4a927d8d00ffb933f03290d842f7a1fae
Prowisest <promisedanielakpan@gmail.com>
2023-08-14 04:22:39 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,6 +10,6 @@ int main(void)
 *
 * Return: Always 0 (success)
 */
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
